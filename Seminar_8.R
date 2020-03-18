d4a0e8d53e7240b670b6a1f4232c18e22b38095f
Eldar Rakhimberdiev <eldarrak@gmail.com>
2020-03-18 12:50:18 +0100

    Update Seminar_8.R

--- Seminar_8.R
+++ Seminar_8.R
@@ -8,3 +8,52 @@ str(Data)
 
 plot(Data)
 
+Data<-Data[,-1]
+Data_no_na<-na.omit(Data)
+str(Data)
+str(Data_no_na)
+
+param.pca<-prcomp(Data_no_na, center=TRUE, scale=TRUE)
+
+plot(param.pca)
+biplot(param.pca, scale=FALSE)
+
+summary(param.pca)
+
+print(param.pca)
+
+# GAM
+
+library(mgcv)
+
+install.packages('gamair')
+library(gamair)
+
+data(cairo)
+str(cairo)
+head(cairo)
+tail(cairo)
+summary(cairo)
+
+plot(temp~time, data=cairo)
+
+#save(cairo, file='cairo.RData')
+
+cairo$tempC<-(cairo$temp-32)/1.8
+
+Gam1<-gam(tempC~time+s(month), data=cairo[1:1000,])
+summary(Gam1)
+
+plot(Gam1)
+
+Gam2<-gam(tempC~time+s(month, bs='cc', k=12), knots=list(month=c(0.5, 12.5)), data=cairo[1:1000,])
+summary(Gam2)
+plot(Gam2)
+
+
+
+
+
+
+
+
